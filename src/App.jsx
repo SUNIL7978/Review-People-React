c27b16effabb46d071545f0bd3265fad05b68138
SUNIL7978 <Sunilprusty16@gmail.com>
2023-08-25 11:25:48 +0530

    Star-Review-React

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import { data } from './data';
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+
+function App() {
+  const [index, setIndex] = useState(0);
+  const { image, name, info, job } = data[index];
+
+  const checkNumber = (number) => {
+    if (number > data.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return data.length - 1;
+    }
+    return number;
+  };
+
+  const nextIndex = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const prevIndex = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+
+      return checkNumber(newIndex);
+    });
+  };
+
+  const randomPerson = () => {
+    let randomIndex = Math.floor(Math.random() * data.length);
+    if (randomIndex === index) {
+      return (randomIndex = index + 1);
+    }
+    setIndex(randomIndex);
+  };
+
+  return (
+    <>
+      <main>
+        <article className="review">
+          <div className="img-container">
+            <img src={image} alt={name} className="person-img" />
+            <span className="quote-icon">
+              <FaQuoteRight />
+            </span>
+          </div>
+          <h5 className="author">{name}</h5>
+          <p className="job">{job}</p>
+          <p className="info">{info}</p>
+          <div className="btn-container">
+            <button type="button" className="prev-btn" onClick={prevIndex}>
+              <FaChevronLeft />
+            </button>
+            <button type="button" className="next-btn" onClick={nextIndex}>
+              <FaChevronRight />
+            </button>
+          </div>
+          <button
+            className="btn btn-hipster"
+            type="button"
+            onClick={randomPerson}
+          >
+            Surprise me
+          </button>
+        </article>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/data.jsx
@@ -0,0 +1,42 @@
+export const data = [
+  {
+    id: 1,
+    name: 'Sundar Pichai',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/d/d6/Sundar_Pichai_%282023%29_cropped.jpg',
+    job: 'CEO of Alphabet',
+    info: "Pichai began his career as a materials engineer. Following a short stint at the management consulting firm McKinsey & Co., Pichai joined Google in 2004,[8] where he led the product management and innovation efforts for a suite of Google's client software products, including Google Chrome and ChromeOS, as well as being largely responsible for Google Drive.",
+  },
+  {
+    id: 2,
+    name: 'Satya Nadella',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/7/78/MS-Exec-Nadella-Satya-2017-08-31-22_%28cropped%29.jpg',
+    job: 'Chairman and CEO, Microsoft',
+    info: 'Satya was born on 19 August 1967 in Hyderabad, Andhra Pradesh, India into a Hindu Telugu Brahmin Nadella family. His mother Prabhavati was a Sanskrit lecturer[12] and his father, Bukkapuram Nadella Yugandhar, was an Indian Administrative Service officer of the 1962 batch. Yugandhar hailed from Bukkapuram in Anantapur district of Andhra Pradesh.',
+  },
+  {
+    id: 3,
+    name: 'Shantanu Narayen',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/0/01/Shantanu_Narayen_-_the_CEO_of_Adobe_Inc.jpg',
+    job: 'Chairman and CEO of Adobe Inc.',
+    info: 'In 1986 Narayen joined a Silicon Valley start-up called Measurex Automation Systems, which made computer control systems for automotive and electronics customers. He then moved to Apple, where he was in senior management positions from 1989 to 1995.',
+  },
+  {
+    id: 4,
+    name: 'Ratan Tata',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/f/f8/Ratan_Tata_photo.jpg',
+    job: 'Industrialist, Philanthropist',
+    info: "In the 1970s, Tata was given a managerial position in the Tata group. He achieved initial success by turning the subsidiary National Radio and Electronics (NELCO) around, only to see it collapse during an economic slowdown.[15][16] In 1991, J. R. D. Tata stepped down as chairman of Tata Sons, naming him his successor. Initially, Tata faced stiff resistance from the heads of various subsidiaries, who had a large amount of operational freedom under the senior Tata's tenure. ",
+  },
+  {
+    id: 5,
+    name: 'S.Somanath',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/9/97/S._Somanath%2C_Director_of_VSSC%2C_ISRO%2C_speaks_during_the_Heads_of_Agency_Plenary_of_the_70th_International_Astronautical_Congress.jpg',
+    job: 'Chairman of the ISRO',
+    info: 'The Indian National Committee for Space Research (INCOSPAR) was founded in 1962 under the Department of Atomic Energy (DAE) with Vikram Sarabhai as its chairperson which in 1969 became ISRO.[4] In 1972, government of India had set up a space commission and DOS and brought ISRO under DOS.[5] ,Since Sarabhai has assumed the position, there have been eleven chairmen of the ISRO, with Satish Dhawan serving the longest term of 12 years as the chairman. ',
+  },
+];
